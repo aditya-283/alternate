a4a84d464915b6f8e90f66695ec3b4692985c2c5
razered <bhagwat.work@gmail.com>
2022-03-24 05:16:52 -0400

    Already downloaded

--- main.py
+++ main.py
@@ -45,14 +45,14 @@ transform_test = transforms.Compose(
 )
 
 trainset = torchvision.datasets.CIFAR10(
-    root="./data", train=True, download=True, transform=transform_train
+    root="./data", train=True, download=False, transform=transform_train
 )
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2
 )
 
 testset = torchvision.datasets.CIFAR10(
-    root="./data", train=False, download=True, transform=transform_test
+    root="./data", train=False, download=False, transform=transform_test
 )
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2
