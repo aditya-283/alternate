a5a4a20d28920bf46eba4a5146b2164313fed15b
razered <bhagwat.work@gmail.com>
2022-03-26 14:25:15 -0400

    Test for CIFAR 100

--- main.py
+++ main.py
@@ -52,32 +52,32 @@ transform_test = transforms.Compose(
     ]
 )
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = torchvision.datasets.CIFAR100(
     root="./data", train=True, download=True, transform=transform_train
 )
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=1
 )
 
-testset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.CIFAR100(
     root="./data", train=False, download=True, transform=transform_test
 )
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=1
 )
 
-classes = (
-    "plane",
-    "car",
-    "bird",
-    "cat",
-    "deer",
-    "dog",
-    "frog",
-    "horse",
-    "ship",
-    "truck",
-)
+# classes = (
+#     "plane",
+#     "car",
+#     "bird",
+#     "cat",
+#     "deer",
+#     "dog",
+#     "frog",
+#     "horse",
+#     "ship",
+#     "truck",
+# )
 
 # Model
 print("==> Building model..")
